3b2c161b7b6f6e6b31f0dac9d5a1fcc80b8c5b5a
ZOOY2ON <zooy2on01@gmail.com>
2022-09-28 02:21:31 +0900

    프로젝트 및 솔루션 이름 변경

---
+++ practice_zip/head_sample.cpp
@@ -0,0 +1,9 @@
+//// 프로그램에 헤더 파일 넣기
+//#include <gl/glew.h>
+//#include <gl/freeglut.h>
+//#include <gl/freeglut_ext.h>
+//
+//// 좌표계 변환 적용할 때 , 아래의 헤더파일 추가로 넣기 : glm 헤더파일 추가 ( 폴더 맞춰서 추가하기 )
+//#include <gl/glm/glm.hpp>
+//#include <gl/glm/ext.hpp>
+//#include <gl/glm/gtc/matrix_transform.hpp>
